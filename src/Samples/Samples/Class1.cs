cd60ffcd4cce7504002f0fdfe715bca509aa831c
Hubert Dzikuc <YOUR_EMAIL>
2024-01-14 17:00:17 +0100

    OneOf

--- src/Samples/Samples/Class1.cs
+++ src/Samples/Samples/Class1.cs
@@ -49,12 +49,78 @@ internal sealed class ExampleMessageConsumer : IExampleMessageConsumer
     }
 }
 
+public record T1Example()
+{
+    public Task Handle()
+    {
+    }
+}
+public record T2Example()
+{
+    public Task Handle()
+    {
+    }
+}
+public record T3Example()
+{
+    public Task Handle()
+    {
+    }
+}
+
+public interface IPart<T>
+{
+    
+}
+
+public interface IOneOf<T1, T2, T3>
+{
+}
+
+public sealed record Is_T1Example(T1Example Value) : IOneOf<T1Example, T2Example, T3Example>, IOneOf<T3Example, T2Example, T1Example>
+{
+    public static explicit operator Is_T1Example(T1Example value)
+    {
+        return new Is_T1Example(value);
+    }
+}
+
+public sealed record Is_T2Example(T2Example Value) : IOneOf<T1Example, T2Example, T3Example>, IOneOf<T3Example, T2Example, T1Example>
+{
+}
+
+public sealed record Is_T3Example(T3Example Value) : IOneOf<T1Example, T2Example, T3Example>, IOneOf<T3Example, T2Example, T1Example>
+{
+}
+
+public static class OneOfExtensions
+{
+    public static IOneOf<T1Example, T2Example, T3Example> ToReturn(T1Example t1)
+    {
+        return (Is_T1Example)t1;
+    }
+}
+
 public sealed class Dependncy
 {
+    public async Task Handle(IOneOf<T1Example, T2Example, T3Example> oneOf)
+    {
+        var task = oneOf switch
+        {
+            Is_T1Example t => t.Value.Handle(),
+            Is_T2Example t => t.Value.Handle(),
+            Is_T3Example t => t.Value.Handle(),
+            _ => Task.CompletedTask
+        };
+
+        await task;
+    }
+
     public static void Register(IServiceCollection services)
     {
         services.AddFeature<IExampleFeatureSlice, ExampleFeatureSlice>();
         services.AddFeature<IExampleMethod, ExampleMethod>();
+        services.AddFeature<IExampleMethod2, ExampleMethod2>();
         services.AddFeature<IExampleMessageConsumer, ExampleMessageConsumer>();
     }
 
