7350eb22a605dc74541f0b5f12a341e6bafcb21b
Hubert Dzikuc <YOUR_EMAIL>
2024-01-14 18:18:54 +0100

    OneOf2 approach, is nicer

--- src/FeatureSlice/FeatureSlice/New/OneOf2.cs
+++ src/FeatureSlice/FeatureSlice/New/OneOf2.cs
@@ -1,44 +1,54 @@
-namespace OneOfExample;
+namespace OneOf2Example;
 
-public record T1Example()
+public partial record T1Example()
 {
     public Task Handle()
     {
         return Task.CompletedTask;
     }
 }
-public record T2Example()
+
+//Auto Generated
+public partial record T1Example : IOneOf<T1Example, T2Example, T3Example>
+{
+
+}
+
+public partial record T2Example()
 {
     public Task Handle()
     {
         return Task.CompletedTask;
     }
 }
-public record T3Example()
+
+//Auto Generated
+public partial record T2Example : IOneOf<T1Example, T2Example, T3Example>
+{
+}
+
+public partial record T3Example()
 {
     public Task Handle()
     {
         return Task.CompletedTask;
     }
+}
+
+//Auto Generated
+public partial record T3Example : IOneOf<T1Example, T2Example, T3Example>
+{
 }
 
 public interface IOneOf<T1, T2, T3>
 {
 }
 
-public sealed record Is<T>(T Value) : IOneOf<T, T2Example, T3Example>, IOneOf<T1Example, T, T3Example>, IOneOf<T1Example, T2Example, T>
-{
-    public static explicit operator Is<T>(T value)
-    {
-        return new Is<T>(value);
-    }
-}
-
 public static class OneOfExtensions
 {
     public static IOneOf<T1Example, T2Example, T3Example> ToReturn(T1Example t1)
     {
-        return (Is<T1Example>)t1;
+        return t1;
     }
 }
 
@@ -48,9 +58,9 @@ public sealed class Dependncy
     {
         var task = oneOf switch
         {
-            Is<T1Example> t => t.Value.Handle(),
-            Is<T2Example> t => t.Value.Handle(),
-            Is<T3Example> t => t.Value.Handle(),
+            T1Example t => t.Handle(),
+            T2Example t => t.Handle(),
+            T3Example t => t.Handle(),
             _ => Task.CompletedTask
         };
 
