6335c5d7ed95e9f56483ef9c8b2caecd8364cf08
Hubert Dzikuc <YOUR_EMAIL>
2024-01-14 21:37:17 +0100

    some-rework

--- src/FeatureSlice/FeatureSlice/New/OneOf2.cs
+++ src/FeatureSlice/FeatureSlice/New/OneOf2.cs
@@ -1,6 +1,6 @@
 namespace OneOf2Example;
 
-public sealed partial record T1Example()
+public partial record T1Example()
 {
     public Task<ExampleReturn> Handle()
     {
@@ -8,7 +8,7 @@ public sealed partial record T1Example()
     }
 }
 
-public sealed partial record T2Example()
+public partial record T2Example()
 {
     public Task<ExampleReturn> Handle()
     {
@@ -16,7 +16,7 @@ public sealed partial record T2Example()
     }
 }
 
-public sealed partial record T3Example()
+public partial record T3Example()
 {
     public Task<ExampleReturn> Handle()
     {
@@ -38,45 +38,46 @@ public interface IOneOfT2<T2>
 
 public interface IOneOf<T0, T1> : IOneOfT0<T0>, IOneOfT1<T1>
 {
-    public class Get : IOneOf<T0, T1>
-    {
-        public static implicit operator Get(T0 value)
-        {
-            return new Get();
-        }
+}
 
-        public static implicit operator Get(T1 value)
-        {
-            return new Get();
-        }
+public sealed class OneOf<T0, T1> : IOneOf<T0, T1>
+{
+    public static implicit operator OneOf<T0, T1>(T0 value)
+    {
+        return new OneOf<T0, T1>();
+    }
+
+    public static implicit operator OneOf<T0, T1>(T1 value)
+    {
+        return new OneOf<T0, T1>();
     }
 }
 
 public interface IOneOf<T0, T1, T2> : IOneOfT0<T0>, IOneOfT1<T1>, IOneOfT2<T2>
 {
-    public class Get : IOneOf<T0, T1, T2>
+}
+
+public sealed class OneOf<T0, T1, T2> : IOneOf<T0, T1, T2>
+{
+    public static implicit operator OneOf<T0, T1, T2>(T0 value)
     {
-        public static implicit operator Get(T0 value)
-        {
-            return new Get();
-        }
+        return new OneOf<T0, T1, T2>();
+    }
 
-        public static implicit operator Get(T1 value)
-        {
-            return new Get();
-        }
+    public static implicit operator OneOf<T0, T1, T2>(T1 value)
+    {
+        return new OneOf<T0, T1, T2>();
+    }
 
-        public static implicit operator Get(T2 value)
-        {
-            return new Get();
-        }
+    public static implicit operator OneOf<T0, T1, T2>(T2 value)
+    {
+        return new OneOf<T0, T1, T2>();
     }
 }
 
-
 public sealed class Dependncy
 {
-    public IOneOf<T1Example, T2Example>.Get Handle(IOneOf<T1Example, T2Example, T3Example> oneOf)
+    public OneOf<T1Example, T2Example> Handle(IOneOf<T1Example, T2Example, T3Example> oneOf)
     {
         return oneOf switch
         {
@@ -102,16 +103,12 @@ public sealed class Dependncy
 
 
 //Auto Generated
-public partial record T1Example : IOneOf<T1Example, T2Example, T3Example>, IOneOf<T1Example, T1Example, T3Example>
+public sealed partial record T1Example : IOneOf<T1Example, T2Example, T3Example>, IOneOf<T1Example, T1Example, T3Example>
 {
 }
-
-//Auto Generated
-public partial record T2Example : IOneOf<T1Example, T2Example, T3Example>, IOneOf<T2Example, T2Example, T3Example>
+public sealed partial record T2Example : IOneOf<T1Example, T2Example, T3Example>, IOneOf<T2Example, T2Example, T3Example>
 {
 }
-
-//Auto Generated
-public partial record T3Example : IOneOf<T1Example, T2Example, T3Example>, IOneOf<T2Example, T2Example, T3Example>
+public sealed partial record T3Example : IOneOf<T1Example, T2Example, T3Example>, IOneOf<T2Example, T2Example, T3Example>
 {
 }
