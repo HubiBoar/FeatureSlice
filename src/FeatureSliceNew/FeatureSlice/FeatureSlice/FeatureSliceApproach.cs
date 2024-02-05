746b389f33bbb897107abc94742da8010885c3ed
Hubert Dzikuc <150520704+HubiBoar@users.noreply.github.com>
2024-02-05 21:52:48 +0100

    FeatureSlice Dependencies approach looks great

--- src/FeatureSliceNew/FeatureSlice/FeatureSlice/FeatureSliceApproach.cs
+++ src/FeatureSliceNew/FeatureSlice/FeatureSlice/FeatureSliceApproach.cs
@@ -16,63 +16,142 @@ public interface IHandler<TRequest, TResponse>
     public Task<OneOf<TRequest, Error>> Handle(TResponse response);
 }
 
-public partial interface IFeatureSliceBase<TSelf, TRequest, TResponse, THandler>
-    where TSelf : IFeatureSliceBase<TSelf, TRequest, TResponse, THandler>
-    where THandler : IHandler<TRequest, TResponse>
+public partial interface IForwardFacingFeatureSlice<TRequest, TResponse>
 {
-}
-
-public abstract partial class FeatureSlice<TSelf, TRequest, TResponse, THandler>
-    where TSelf : IFeatureSliceBase<TSelf, TRequest, TResponse, THandler>
-    where THandler : IHandler<TRequest, TResponse>
-{
-    public delegate Task<OneOf<TRequest, Error>> Dispatch(TResponse request);
-
-    public abstract partial class WithToggle : IFeatureSliceBase<TSelf, TRequest, TResponse, THandler>
+    public static class Delegate
     {
-        public delegate Task<OneOf<TRequest, Disabled, Error>> Dispatch(TResponse request);
 
-        public abstract string Name { get; }
+        public abstract partial class Default : IForwardFacingFeatureSlice<TRequest, TResponse>
+        {
+            public delegate Task<OneOf<TRequest, Error>> Dispatch(TResponse request);
 
-        public abstract partial class WithEndpoint<TEndpoint> : IFeatureSliceBase<TSelf, TRequest, TResponse, THandler>
-            where TEndpoint : Feature.IEndpoint
+            protected static void RegisterInternal(IServiceCollection services, Func<IServiceProvider, Dispatch> dispatcher)
+            {
+                services.AddSingleton<Dispatch>(provider => dispatcher(provider));
+            }
+        }
+
+        public abstract partial class Toggle : IForwardFacingFeatureSlice<TRequest, TResponse>
         {
             public delegate Task<OneOf<TRequest, Disabled, Error>> Dispatch(TResponse request);
 
             public abstract string Name { get; }
 
-            public static void Register(IServiceCollection services, IConfiguration configuration, HostExtender<WebApplication> hostExtender)
+            protected static void RegisterInternal(IServiceCollection services, Func<IServiceProvider, Dispatch> dispatcher)
             {
-                WithToggle.Register(services, configuration);
+                services.AddSingleton<Dispatch>(provider => dispatcher(provider));
+            }
+        }
+    }
+
+    public static class Handler
+    {
+        public abstract partial class Default<THandler> : Delegate.Default
+            where THandler : class, IHandler<TRequest, TResponse>
+        {
+            protected static void RegisterInternal(IServiceCollection services)
+            {
+                services.AddSingleton<THandler>();
+                RegisterInternal(services, provider => provider.GetRequiredService<THandler>().Handle);
+            }
+        }
+
+        public abstract partial class Toggle<THandler> : Delegate.Toggle
+            where THandler : class, IHandler<TRequest, TResponse>
+        {
+            protected static void RegisterInternal(IServiceCollection services)
+            {
+                services.AddSingleton<THandler>();
+                //Check if is enabled etc
+                //Register(services, provider => provider.GetRequiredService<THandler>().Handle);
+            }
+        }
+    }
+
+    public static class Endpoint
+    {
+        public abstract partial class Default<TEndpoint> : IForwardFacingFeatureSlice<TRequest, TResponse>
+            where TEndpoint : Feature.IEndpoint
+        {
+            protected static void RegisterInternal(HostExtender<WebApplication> hostExtender)
+            {
                 hostExtender.Map<TEndpoint>();
             }
         }
 
-        public static void Register(IServiceCollection services, IConfiguration configuration)
+        public abstract partial class Toggle<TEndpoint> : IForwardFacingFeatureSlice<TRequest, TResponse>
+            where TEndpoint : Feature.IEndpoint
         {
+            public abstract string Name { get; }
+
+            protected static void RegisterInternal(HostExtender<WebApplication> hostExtender)
+            {
+                hostExtender.Map<TEndpoint>();
+            }
+        }
+    }
+}
+
+public static partial class FeatureSlice<TSelf, TRequest, TResponse>
+    where TSelf : IForwardFacingFeatureSlice<TRequest, TResponse>
+{
+    public static partial class WithToggle
+    {
+        public abstract partial class WithEndpoint<TEndpoint> : IForwardFacingFeatureSlice<TRequest, TResponse>.Endpoint.Toggle<TEndpoint>
+            where TEndpoint : Feature.IEndpoint
+        {
+            public static void Register(HostExtender<WebApplication> hostExtender)
+            {
+                RegisterInternal(hostExtender);
+            }
+
+            public abstract partial class WithHandler<THandler> : IForwardFacingFeatureSlice<TRequest, TResponse>.Handler.Toggle<THandler>
+                where THandler : class, IHandler<TRequest, TResponse>
+            {
+                public static void Register(IServiceCollection services, HostExtender<WebApplication> hostExtender)
+                {
+                    WithEndpoint<TEndpoint>.Register(hostExtender);
+                    RegisterInternal(services);
+                }
+            }
+        }
+
+        public abstract partial class WithHandler<THandler> : IForwardFacingFeatureSlice<TRequest, TResponse>.Handler.Toggle<THandler>
+            where THandler : class, IHandler<TRequest, TResponse>
+        {
+            public static void Register(IServiceCollection services)
+            {
+                RegisterInternal(services);
+            }
         }
     }
 
-    public abstract partial class WithEndpoint<TEndpoint> : IFeatureSliceBase<TSelf, TRequest, TResponse, THandler>
-        where TEndpoint : FeatureSlice.Feature.IEndpoint
+    public abstract partial class WithEndpoint<TEndpoint> :  IForwardFacingFeatureSlice<TRequest, TResponse>.Endpoint.Default<TEndpoint>
+        where TEndpoint : Feature.IEndpoint
     {
-        public static void Register(IServiceCollection services, IConfiguration configuration)
+        public static void Register(HostExtender<WebApplication> hostExtender)
         {
+            RegisterInternal(hostExtender);
         }
     }
 
-    public static void Register(IServiceCollection services, IConfiguration configuration)
+    public abstract partial class WithHandler<THandler> : IForwardFacingFeatureSlice<TRequest, TResponse>.Handler.Default<THandler>
+        where THandler : class, IHandler<TRequest, TResponse>
     {
+        public static void Register(IServiceCollection services)
+        {
+            RegisterInternal(services);
+        }
     }
 }
 
 public sealed class ExampleFeature : FeatureSlice<
     ExampleFeature,
     ExampleFeature.Request,
-    ExampleFeature.Response,
-    ExampleFeature.Handler>
+    ExampleFeature.Response>
     .WithToggle
     .WithEndpoint<ExampleFeature.Endpoint>
+    .WithHandler<ExampleFeature.Handler>
 {
     public record Request();
     public record Response();
@@ -87,10 +166,10 @@ public sealed class ExampleFeature : FeatureSlice<
         }
     }
 
-    public class Endpoint : FeatureSlice.Feature.IEndpoint
+    public class Endpoint : Feature.IEndpoint
     {
-        public static FeatureSlice.Feature.EndpointSetup Setup =>
-            FeatureSlice.Feature.EndpointSetup.MapPut("test", () => {});
+        public static Feature.EndpointSetup Setup =>
+            Feature.EndpointSetup.MapPut("test", () => {});
     }
 }
 
@@ -101,9 +180,9 @@ public class Usage
         dispatch.Invoke(new ExampleFeature.Response());
     }
 
-    public static void Register(IServiceCollection services, IConfiguration configuration)
+    public static void Register(IServiceCollection services, HostExtender<WebApplication> hostExtender)
     {
-        ExampleFeature.Register(services, configuration);
+        ExampleFeature.Register(services, hostExtender);
     }
 }
 
